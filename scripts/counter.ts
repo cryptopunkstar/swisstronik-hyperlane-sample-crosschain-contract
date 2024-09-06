85c923ed66045466cf9cbf43ab82fd34f4210fb1
Cryptopunkstar <148623328+cryptopunkstar@users.noreply.github.com>
2024-09-06 23:54:53 +0200

    Update counter.ts

--- scripts/counter.ts
+++ scripts/counter.ts
@@ -2,15 +2,15 @@ import {ethers} from "hardhat";
 
 
 async function main() {
-  // Construct instance of SampleCrossChainCounter in Mumbai
-  const provider = new ethers.JsonRpcProvider(process.env.MUMBAI_RPC);
+  // Construct instance of SampleCrossChainCounter in Amoy
+  const provider = new ethers.JsonRpcProvider(process.env.AMOY_RPC);
   const wallet = new ethers.Wallet(process.env.DEPLOYER_KEY!);
   const signer = wallet.connect(provider);
 
-  const contract = await ethers.getContractAt("SampleCrossChainCounter", process.env.MUMBAI_CONTRACT_ADDRESS!, signer);
+  const contract = await ethers.getContractAt("SampleCrossChainCounter", process.env.AMOY_CONTRACT_ADDRESS!, signer);
 
   const counterValue = await contract.counter();
-  console.log('Counter value in Mumbai: ', counterValue.toString());
+  console.log('Counter value in Amoy: ', counterValue.toString());
 }
 
 main().catch((error) => {
