195d45368e2a1306e1dd63413bb2028726ee1600
Cryptopunkstar <148623328+cryptopunkstar@users.noreply.github.com>
2024-09-06 23:53:48 +0200

    Update deploy.ts
    deprecated mumbai against amoy testnet polygon

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,16 +2,16 @@ import { ethers } from "hardhat";
 import { encryptDataField } from "@swisstronik/swisstronik.js";
 
 const SWISSTRONIK_CHAIN_ID = 1291;
-const MUMBAI_CHAIN_ID = 80001;
+const AMOY_CHAIN_ID = 80001;
 
 const SWISSTRONIK_MAILBOX = "0xF12c1fA2ca4a0EB6051591B57250Cf44bFd242Bf";
-// We're using empty addresses for Swisstronik since Mumbai can be understood by default hook
+// We're using empty addresses for Swisstronik since Amoy can be understood by default hook
 const SWISSTRONIK_MERKLE_TREE_HOOK = ethers.ZeroAddress;
 const SWISSTRONIK_ISM = "0x6289Ab10eE0Ff1bF222e740A7AE3FF79A9e27110";
 
-const MUMBAI_MERKLE_TREE_HOOK = "0x9AF85731EDd41E2E50F81Ef8a0A69D2fB836EDf9";
-const MUMBAI_MAILBOX = "0x2d1889fe5B092CD988972261434F7E5f26041115";
-const MUMBAI_ISM = "0xb27a1acE410bc221039225276255Aff8ef8d2b1C";
+const AMOY_MERKLE_TREE_HOOK = "0x9AF85731EDd41E2E50F81Ef8a0A69D2fB836EDf9";
+const AMOY_MAILBOX = "0x2d1889fe5B092CD988972261434F7E5f26041115";
+const AMOY_ISM = "0xb27a1acE410bc221039225276255Aff8ef8d2b1C";
 
 async function main() {
   if (!process.env.DEPLOYER_KEY) {
@@ -21,9 +21,9 @@ async function main() {
 
   console.log('Preparing signers for both networks');
   const wallet = new ethers.Wallet(process.env.DEPLOYER_KEY);
-  const mumbaiProvider = new ethers.JsonRpcProvider(process.env.MUMBAI_RPC);
+  const amoyProvider = new ethers.JsonRpcProvider(process.env.AMOY_RPC);
   const swisstronikProvider = new ethers.JsonRpcProvider(process.env.SWISSTRONIK_RPC);
-  const mumbaiSigner = wallet.connect(mumbaiProvider);
+  const amoySigner = wallet.connect(amoyProvider);
   const swisstronikSigner = wallet.connect(swisstronikProvider);
 
   console.log('Deploying SampleCrossChainCounter to Swisstronik');
@@ -32,33 +32,33 @@ async function main() {
     [
       SWISSTRONIK_MAILBOX,
       SWISSTRONIK_MERKLE_TREE_HOOK,
-      MUMBAI_CHAIN_ID, // destination chain id,
+      AMOY_CHAIN_ID, // destination chain id,
       SWISSTRONIK_ISM,
     ],
     swisstronikSigner,
   );
   await swtrContract.waitForDeployment();
 
-  console.log('Deploying SampleCrossChainCounter to Polygon Mumbai');
-  const mumbaiContract = await ethers.deployContract(
+  console.log('Deploying SampleCrossChainCounter to Polygon Amoy');
+  const amoyContract = await ethers.deployContract(
     "SampleCrossChainCounter",
     [
-      MUMBAI_MAILBOX,
-      MUMBAI_MERKLE_TREE_HOOK,
+      AMOY_MAILBOX,
+      AMOY_MERKLE_TREE_HOOK,
       SWISSTRONIK_CHAIN_ID, // destination chain id
-      MUMBAI_ISM,
+      AMOY_ISM,
     ],
-    mumbaiSigner,
+    amoySigner,
   );
-  await mumbaiContract.waitForDeployment();
+  await amoyContract.waitForDeployment();
 
   console.log('Configure recipient address in Swisstronik');
   const swisstronikContractAddress = await swtrContract.getAddress();
-  const mumbaiContractAddress = await mumbaiContract.getAddress();
+  const amoyContractAddress = await amoyContract.getAddress();
 
   const encodedSwtrTxData = swtrContract.interface.encodeFunctionData(
     "setCounterContractInOtherChain",
-    [mumbaiContractAddress]
+    [amoyContractAddress]
   );
 
   await sendShieldedTransaction(
@@ -68,12 +68,12 @@ async function main() {
     0
   );
 
-  console.log('Configure recipient address in Polygon Mumbai');
-  await mumbaiContract.setCounterContractInOtherChain(swisstronikContractAddress);
+  console.log('Configure recipient address in Polygon Amoy');
+  await amoyContract.setCounterContractInOtherChain(swisstronikContractAddress);
 
   console.log('Contracts were deployed');
   console.log('Swisstronik: ', swisstronikContractAddress);
-  console.log('Mumbai: ', mumbaiContractAddress);
+  console.log('Amoy: ', amoyContractAddress);
 }
 
 const sendShieldedTransaction = async (
